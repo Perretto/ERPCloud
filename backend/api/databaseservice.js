96668530d83fae82a243262faa6595c247d14507
André Perretto de Andrade <andreperretto@hotmail.com>
2018-03-27 15:25:34 -0300

    ajuste servidor

--- backend/api/databaseservice.js
+++ backend/api/databaseservice.js
@@ -10,8 +10,8 @@ const router = express.Router()
 server.use('/api', router)
 // config for your database
 //var config = {user: 'sa', password: 'IntSql2015@', server: '52.89.63.119',  database: 'eCloud-homologa'};
-//var config = {user: 'sa', password: 'IntSql2015@', server: '172.31.8.216',  database: 'eCloud-homologa'};
-var config = {user: 'sa', password: '1234567890', server: '127.0.0.1',  database: 'eCloud-homologa'};
+var config = {user: 'sa', password: 'IntSql2015@', server: '172.31.8.216',  database: 'eCloud-homologa'};
+//var config = {user: 'sa', password: '1234567890', server: '127.0.0.1',  database: 'eCloud-homologa'};
 
 var EnterpriseID = "";
 var UserID = "";
