37ded69bc169cb93edd8549633307cb521ac296f
perretto <andreperretto@hotmail.com>
2018-09-10 13:34:00 -0300

    bkp

--- backend/api/databaseservice.js
+++ backend/api/databaseservice.js
@@ -325,8 +325,14 @@ function compareObj(a,b) {
                                 var options = {paginationOffset: 1,orientation: orientation, header: {"height": "" + headersize + "mm", "contents": html.header} ,footer: {"height":"10mm", "contents":html.footer} };
                                 //var options = {};
                                 pdf.create(html.topo +  html.detail + html.footer + html.base, options).toStream(function(err, stream){
-                                    console.log(fs.createWriteStream('../frontend/reports/' + nome + '.pdf'))
-                                    stream.pipe(fs.createWriteStream('../frontend/reports/' + nome + '.pdf'));
+                                    //console.log(fs.createWriteStream('../frontend/reports/' + nome + '.pdf'))
+                                    if(local == true){
+                                        stream.pipe(fs.createWriteStream('../frontend/reports/' + nome + '.pdf'));
+                                    }else{
+                                        stream.pipe(fs.createWriteStream('/home/ubuntu/ERPCloud/frontend/reports/' + nome + '.pdf'));
+                                    }
+                                    
+
                                     res.setHeader('Content-type', 'application/pdf')
                                     stream.pipe(res)
                                 });
